34651e79f553f857a213d40c2cda32d53dfa3bc8
Akshay <khullarakshay723@gmail.com>
2024-11-06 12:04:08 +0530

    number

--- milane/src/components/Reservation.jsx
+++ milane/src/components/Reservation.jsx
@@ -120,7 +120,7 @@ const Reservation = () => {
               href="tel:0146662579"
               className="body-1 contact-number hover-underline"
             >
-              - 0146 662 579
+              - 01 46 66 25 79
             </a>
             <div className="separator" />
             <p className="contact-label">Location</p>
